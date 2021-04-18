d42192b69f184bc75465c58429756567621e7745
Furkan Meclis <gamefurkanmeclis@gmail.com>
2021-04-18 17:26:02 +0300

    Upload Files

--- src/components/Detay.vue
+++ src/components/Detay.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="container mb-5">
+
     <h1>{{post.title}} <router-link to="/" class="btn btn-warning ms-3">Anasayfa</router-link></h1><br>
     <img :src="'https://www.themoviedb.org/t/p/original'+ post.poster_path" class="rounded" :alt="post.title" style="width: 300px;"><br>
 
     <p>{{post.overview}}</p>
-    <p><a class="btn btn-info" :href="post.homepage" target="_blank">Filmi İzle</a>
+    <p><a v-if="post.homepage" class="btn btn-info" :href="post.homepage" target="_blank">Filmi İzle</a>
 
     </p>
     <p><a :href="'https://www.imdb.com/title/'+post.imdb_id" target="_blank" class="btn btn-primary">IMDB</a></p>
@@ -31,6 +32,7 @@ export default {
       errors: []
     };
   },
+
   methods:{
     poster(){
       let ids = this.$route.params.id;
